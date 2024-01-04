4ac620b75aaf637cdb698b3c17c49d8f4310d0e1
maxLiu <neoliukai@gmail.com>
2024-01-04 16:19:54 +0800

    feat: add reverseArray method and refactor code in Main.java (main)
    This commit adds a new method to reverse an array and refactors some existing code in the Main.java file. The changes are:
    
    - Add reverseArray method that takes an int array as input and returns a new array with the elements in reverse order
    - Use reverseArray method in the main method to print the reversed array
    - Format the code with spaces and comments for readability
    - Remove unused imports and variables
    - Update the workspace.xml file with the latest changelist and task information
    
    The issue number is obtained from the branch name, which is main.

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,49 @@
 public class Main {
     public static void printEvery3() {
         int i = 1;
-        while (i<=88) {
+        while (i <= 88) {
             System.out.println(i);
-            i+=3;
+            i += 3;
         }
     }
-    public static void ninetable(){
-        for (int i = 1; i <=9; i++) {
+
+    public static void ninetable() {
+        for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
-                int m = i*j;
-                System.out.println(i+" x "+j+" = "+ m);
+                int m = i * j;
+                System.out.println(i + " x " + j + " = " + m);
             }
         }
     }
-    public static int findMax(int[] arr){
+
+    public static int findMax(int[] arr) {
         int i = 1;
         int max = arr[0];
         while (i < arr.length) {
-            if(max<arr[i]){
+            if (max < arr[i]) {
                 max = arr[i];
             }
             i++;
         }
         return max;
     }
+    public static int[] reverseArray(int[] arr) {
+        int[] result = new int[arr.length];
+        int length = arr.length;
+        for (int i = 0; i < length ; i++) {
+            result[i] = arr[length-1-i];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-//        printEvery3();
-//        ninetable();
+        // printEvery3();
+        // ninetable();
         int[] array = {4, 3, 6, 7, 9, 10, 33, 0, -4, 12, 400, 133, 324, 1234231, 12334};
-        System.out.println(findMax(array));
+        int[] reversedArray = reverseArray(array);
+        for (int i : reversedArray) {
+            System.out.println(i);
+        }
+//        System.out.println(findMax(array));
     }
 }
